a764b9427fc4cf9261404790a90ad40b000001a8
davilcl <davirasenga@gmail.com>
2024-05-23 14:00:04 -0300

    Adicionando comentario

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-
+#Começando bem
 @app.route('/')
 def index():
     return render_template('index.html')
